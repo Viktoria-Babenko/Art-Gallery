0d55200b5e1a95d0dd9265d599ec7b926be46cfc
Виктория <babenko.viktoria.v@gmail.com>
2024-05-09 02:17:44 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+﻿import {Component, OnInit, ViewChild, Output, EventEmitter} from '@angular/core';
+import {DataService} from './data.service';
+import { DataComponent} from "./data.component"
+import {Painting} from './Painting';
+
+@Component({
+    selector: 'my-app',
+    template: `<div class="div">
+                  <div *ngFor="let painting of paintings; let i = index" >
+                    <img class="input" src="{{painting.image}}" (click)="onSelect(i)" >
+                  </div>
+               </div>
+               <data-comp ></data-comp>`,
+    styleUrls: ['./app.component.css']
+})
+
+export class AppComponent implements OnInit { 
+    
+    @ViewChild(DataComponent)
+    private paint: DataComponent;
+
+    paintings: Painting[] = [];
+
+    constructor(private dataService: DataService){}
+    @Output() onChanged = new EventEmitter<number>();
+    ngOnInit(){
+        this.paintings = this.dataService.getData();
+    }
+    onSelect(p : any ){
+
+        this.paint.SetContmp(this.paintings[p]);
+    }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+﻿// Данный модуль является входной точкой в приложение.
+// С помощью директив import импортируется ряд необходимых модулей.
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { DataComponent }   from './data.component';
+import {DataService} from './data.service';
+
+// Функция-декоратор @NgModule ассоциирует метаданные с классом модуля AppModule.
+@NgModule({
+    imports: [BrowserModule, FormsModule],
+    declarations: [AppComponent, DataComponent],
+	providers: [DataService], // регистрация сервисов
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
